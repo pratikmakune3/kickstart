252c67ef53939597d12c241af42f78b086f4f89c
Pratik Makune <pratikmakune3@users.noreply.github.com>
2018-09-21 10:15:00 +0530

    Adding next-routes and including routes for Link wrapper for dynamic routing

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
 	return (
 		<Menu style={{ marginTop : '20px' }}>
-			<Menu.Item>
-				KickstartCoin
-			</Menu.Item>
+			{/*
+				Need to use Link so can't use Menu.Item, it causes style mismatch.
+				Menu.Item & Link are not css style compatible.
+
+				<Menu.Item>
+					KickstartCoin
+				</Menu.Item>
+			*/}
+
+			<Link route='/'>
+				<a className="item">KickstartCoin</a>
+			</Link>
 
 			<Menu.Menu position="right">
-				<Menu.Item>
-					Campaigns
-				</Menu.Item>
 
-				<Menu.Item>
-					+
-				</Menu.Item>
+				<Link route='/'>
+					<a className="item">Campaigns</a>
+				</Link>
+
+				<Link route='/campaigns/new'>
+					<a className="item">+</a>
+				</Link>
+
 			</Menu.Menu>
 		</Menu>
 	)
